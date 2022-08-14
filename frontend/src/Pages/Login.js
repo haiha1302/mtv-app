e75c9276d9929d5ed3277f22e2a8ffbca61c9654
Nguyen Hai Ha <td12a11haiha@gmail.com>
2022-08-14 11:40:29 +0700

    update login

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -28,7 +28,7 @@ const Login = () => {
     const config = { "Content-type": "application/json" };
 
     const res = await axios.post(
-      "http://localhost:5000/api/google-login",
+      "/google-login",
       { token: googleData.tokenId },
       config
     );
